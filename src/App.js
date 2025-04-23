23532e3c15c96ac7d91327597ce7a51b7b6f05df
Prathamesh Lakare <prathameshlakare001@gmail.com>
2025-04-23 14:16:36 +0530

    feat: add login and register routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import "./styles.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+} from "react-router-dom";
 import PostView from "./features/post/PostView";
 import CreatePost from "./pages/CreatePost";
 import MyProfile from "./features/user/MyProfile";
@@ -7,21 +13,27 @@ import UsersProfile from "./pages/UsersProfile";
 import Users from "./features/user/Users";
 import ExplorePage from "./pages/ExplorePage";
 import Bookmark from "./pages/Bookmark";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
-export default function App() {
+function Layout() {
+  const location = useLocation();
+  const isAuthPage =
+    location.pathname === "/login" || location.pathname === "/register";
+
   return (
     <div>
-      <Router>
-        <div>
-          <nav className="navbar navbar-expand-lg bg-white">
-            <div className="container ">
-              <a className="navbar-brand text-danger fw-bold fs-2" href="/">
-                So<span className="text-primary">Media</span>
-              </a>
-            </div>
-          </nav>
-          <div className="container py-3">
-            <div className="row">
+      <div>
+        <nav className="navbar navbar-expand-lg bg-white">
+          <div className="container ">
+            <a className="navbar-brand text-danger fw-bold fs-2" href="/">
+              So<span className="text-primary">Media</span>
+            </a>
+          </div>
+        </nav>
+        <div className="container py-3">
+          <div className="row">
+            {!isAuthPage && (
               <div className="col-md-3">
                 <ul className="nav flex-column">
                   <li className="nav-item mb-3">
@@ -63,23 +75,34 @@ export default function App() {
                   </li>
                 </ul>
               </div>
-              <div className="col-md-5 py-3">
-                <Routes>
-                  <Route path="/" element={<PostView />} />
-                  <Route path="/explore" element={<ExplorePage />} />
-                  <Route path="/myProfile" element={<MyProfile />} />
-                  <Route path="/profile/:userId" element={<UsersProfile />} />
-                  <Route path="/createPost" element={<CreatePost />} />
-                  <Route path="/bookmark" element={<Bookmark />} />
-                </Routes>
-              </div>
+            )}
+            <div className={isAuthPage ? "col-md-12 py-3" : "col-md-5 py-3"}>
+              <Routes>
+                <Route path="/" element={<PostView />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/explore" element={<ExplorePage />} />
+                <Route path="/myProfile" element={<MyProfile />} />
+                <Route path="/profile/:userId" element={<UsersProfile />} />
+                <Route path="/createPost" element={<CreatePost />} />
+                <Route path="/bookmark" element={<Bookmark />} />
+              </Routes>
+            </div>
+            {!isAuthPage && (
               <div className="col-md-3">
                 <Users />
               </div>
-            </div>
+            )}
           </div>
         </div>
-      </Router>
+      </div>
     </div>
+  );
+}
+export default function App() {
+  return (
+    <Router>
+      <Layout />
+    </Router>
   );
 }

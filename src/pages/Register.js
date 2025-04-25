3f13ac5267bc07fbecf71673a865d303879553f2
Prathamesh Lakare <prathameshlakare001@gmail.com>
2025-04-25 11:56:10 +0530

    feat: add status and error alerts

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,34 +1,57 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import { registerUser } from "../features/auth/authSlice";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { clearError, registerUser } from "../features/auth/authSlice";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function Register() {
   const dispatch = useDispatch();
-  const { status, error } = useSelector((state) => state.auth);
-  console.log(status);
+  const navigate = useNavigate();
+  const { status, error, user } = useSelector((state) => state.auth);
   const [formData, setFormData] = useState({
     username: "",
     email: "",
     password: "",
   });
+  const [message, setMessage] = useState("");
+  const [isError, setIsError] = useState(false);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleRegister = async (e) => {
+  const handleRegister = (e) => {
     e.preventDefault();
     dispatch(registerUser(formData));
   };
+
+  useEffect(() => {
+    if (status === "success" && user && Object.keys(user).length > 0) {
+      setMessage("Registration successful!");
+      setIsError(false);
+      setTimeout(() => {
+        setMessage("");
+        navigate("/");
+      }, 2000);
+    } else if (status === "error" && error) {
+      setMessage(`Error: ${error}`);
+      setIsError(true);
+      setTimeout(() => {
+        setMessage("");
+        dispatch(clearError());
+      }, 3000);
+    }
+  }, [status, user, error, navigate, dispatch]);
 
   return (
     <div className="container mt-5" style={{ maxWidth: "500px" }}>
       <h2 className="mb-4">Register</h2>
       {status === "loading" && <p>Loading...</p>}
-      {status === "error" && <p>Error: {error}</p>}
-      {status === "success" && <p>User register successfull.</p>}
+      {message && (
+        <p className={isError ? "alert alert-danger" : "alert alert-success"}>
+          {message}
+        </p>
+      )}
+
       <form onSubmit={handleRegister}>
         <div className="mb-3">
           <label className="form-label">Username</label>

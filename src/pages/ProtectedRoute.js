15bccf9849369ba8ec4e2b51754906a488a82ff9
Prathamesh Lakare <prathameshlakare001@gmail.com>
2025-04-25 11:56:46 +0530

    feat: made Protected route component

---
+++ src/pages/ProtectedRoute.js
@@ -0,0 +1,27 @@
+import { Navigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchUserData } from "../features/auth/authSlice";
+
+const ProtectedRoute = ({ children }) => {
+  const dispatch = useDispatch();
+  const { user, status } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (status === "idle") {
+      dispatch(fetchUserData());
+    }
+  }, [status, dispatch]);
+
+  if (status === "loading" || status === "idle") {
+    return <div>Loading...</div>;
+  }
+
+  if (!user || Object.keys(user).length === 0) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
